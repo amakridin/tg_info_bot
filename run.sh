6853625c9993ca351e437fdc54767f89b75bbc32
alex <amakridin@gmail.com>
2023-12-27 03:09:47 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python3.10 -m venv /root/py/common_tg_bot/venv
+python3.10 -m venv /root/py/tg_info_bot/venv
 source venv/bin/activate
 pip install -r requirements.txt
-nohup python3.10 run_event_listener.py &>/log_listener.log &
-nohup python3.10 run_event_processing.py &>log_processing.log &
+nohup python3.10 run_tg_info_bot_listener.py &>/log_listener.log &
+nohup python3.10 run_tg_info_bot_processing.py &>log_processing.log &
